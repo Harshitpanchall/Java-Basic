d8c97a6d90d4a5c4ef7e3b9581569a6e39f71d2d
Harshitpanchall <harshitpanchal.ind@gmail.com>
2025-12-31 23:11:51 +0530

    Done

---
+++ JavaBasic/src/in/co/raysBasicProgram/JavaHelloProgram.java
@@ -0,0 +1,11 @@
+package in.co.raysBasicProgram;
+
+public class JavaHelloProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello World");
+
+	}
+
+}
---
+++ JavaBasic/src/in/co/raysBasicProgram/TwoSumJava.java
@@ -0,0 +1,16 @@
+package in.co.raysBasicProgram;
+
+public class TwoSumJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 21;
+		int c = a + b;
+		
+		System.out.println("sm :" + c);
+
+	}
+
+}
