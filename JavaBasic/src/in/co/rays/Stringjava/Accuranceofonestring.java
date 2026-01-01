69df54ebb5c84dfd81c1d7bd01622ae983c18047
Harshitpanchall <harshitpanchal.ind@gmail.com>
2026-01-01 15:34:47 +0530

    Done

---
+++ JavaBasic/src/in/co/rays/Stringjava/Accuranceofonestring.java
@@ -0,0 +1,47 @@
+package in.co.rays.Stringjava;
+
+public class Accuranceofonestring {
+
+	public static void main(String[] args) {
+		
+		String name = "Hellooooooooo Harshit1234";
+		
+		String os = "";
+		String result ="";
+		String rev = "";
+		
+		for(int i = 0; i < name.length(); i++) {
+			char ch = name.charAt(i);
+			
+			if(Character.isDigit(ch)) {
+				result = result + ch;
+				
+			}
+			
+			if(ch == 'o') {
+				os = os + ch;
+				
+			}
+			
+		}
+	
+	
+
+		/// reverse split///
+			String[] str = name.split(" ");
+			
+			for(int k = 0; k <= name.length(); k++) {
+				
+				for(int l = str[k].length()-1; l >= 0; l--) {
+					System.out.print(str[k].charAt(l));
+				
+				}
+				System.out.print(" ");
+		
+			}
+			System.out.println(os);
+			System.out.println(result);
+	}
+}
+
+
---
+++ JavaBasic/src/in/co/rays/Stringjava/Stringarray.java
@@ -0,0 +1,19 @@
+package in.co.rays.Stringjava;
+
+public class Stringarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] arr = new String[4];
+		
+		arr[0] = "harshit";
+		arr[1] = "amam";
+		arr[2] = "harsh";
+		System.out.println("String arr :" + arr[0] + "" + arr[1] + "" + arr[2]);
+		
+		
+
+	}
+
+}
