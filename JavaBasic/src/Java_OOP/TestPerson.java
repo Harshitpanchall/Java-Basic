9da3a19107967cf9592cba6decc34437f4c0f8d5
Harshitpanchall <harshitpanchal.ind@gmail.com>
2026-01-02 22:38:27 +0530

    Done

---
+++ JavaBasic/src/Java_OOP/TestPerson.java
@@ -0,0 +1,23 @@
+package Java_OOP;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class TestPerson {
+	public static void main(String[] args) throws ParseException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+		Person p = new Person();
+
+		p.setName("harshit");
+		p.setAddress("bhopal");
+		p.setDob(sdf.parse("23-04-2003"));
+
+		System.out.println("name : " + p.getName());
+		System.out.println("addres : " + p.getAddress());
+		System.out.println("Date : " + p.getDob());
+
+	}
+
+}
